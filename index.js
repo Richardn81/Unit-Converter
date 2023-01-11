0ce0e01797e02ed49b8de34e6b9de74314f5007e
Richardn81 <116152430+Richardn81@users.noreply.github.com>
2023-01-11 00:16:17 -0600

    1st Unit converter

---
+++ index.js
@@ -0,0 +1,24 @@
+let inputEl = document.getElementById("input")
+let convertBtn = document.getElementById("convert-btn")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
+
+convertBtn.addEventListener("click", function(){
+    let baseValue = inputEl.value
+    inputEl.value = ""
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | 
+                            ${baseValue} feet   = ${(baseValue / meterToFeet).toFixed(3)} meter
+    `
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons |
+                            ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters
+    `
+    massEl.textContent = `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | 
+                          ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos
+    `
+})
